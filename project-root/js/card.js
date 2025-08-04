8f1aa4379f8aa9fb3f643c9c854a110750ce12e1
jeonghunpark1 <p500wh@naver.com>
2025-08-04 10:36:31 +0900

    Feat: card 클릭시 video페이지 이동

--- project-root/js/card.js
+++ project-root/js/card.js
@@ -4,32 +4,134 @@ export function loadVideoCards() {
 
   const videoDataList = [
     {
-      thumbnail: "https://i.ytimg.com/vi/3T2s3f2Zk6Q/hqdefault.jpg",
-      title: "냉장고를 부탁해",
-      channelIcon: "/assets/icons/gudock.png",
-      channelName: "호호호",
-      views: "조회수 1.2만회",
-      uploaded: "2일 전"
-    },
-    {
-      thumbnail: "https://i.ytimg.com/vi/HbBjHIwtjlI/hq720.jpg",
-      title: "[Playlist] 지친 당신을 위로해 줄 새벽 감성 유스케 ",
+      videoId: "_piHYoqMJUs",
+      thumbnail: "https://i.ytimg.com/vi/_piHYoqMJUs/hqdefault.jpg",
+      title: "제목",
       channelIcon: "/assets/icons/gudock.png",
       channelName: "채널",
-      views: "조회수 3.1만회",
+      views: "조회수",
+      uploaded: "1일 전"
+    },
+    {
+      videoId: "0poey19dA0c",
+      thumbnail: "https://i.ytimg.com/vi/0poey19dA0c/hqdefault.jpg",
+      title: "제목",
+      channelIcon: "/assets/icons/gudock.png",
+      channelName: "채널",
+      views: "조회수",
       uploaded: "3일 전"
     },
     {
-      thumbnail: "https://i.ytimg.com/vi/_CGsOkBOOr8/hq720.jpg",
-      title: "런닝맨",
+      videoId: "-Q5O71sOM_0",
+      thumbnail: "https://i.ytimg.com/vi/-Q5O71sOM_0/hqdefault.jpg",
+      title: "제목",
       channelIcon: "/assets/icons/gudock.png",
-      channelName: "하하하",
-      views: "조회수 128만회",
+      channelName: "채널",
+      views: "조회수",
+      uploaded: "5일 전"
+    }, {
+      videoId: "0poey19dA0c",
+      thumbnail: "https://i.ytimg.com/vi/0poey19dA0c/hqdefault.jpg",
+      title: "제목",
+      channelIcon: "/assets/icons/gudock.png",
+      channelName: "채널",
+      views: "조회수",
+      uploaded: "3일 전"
+    },
+    {
+      videoId: "-Q5O71sOM_0",
+      thumbnail: "https://i.ytimg.com/vi/-Q5O71sOM_0/hqdefault.jpg",
+      title: "제목",
+      channelIcon: "/assets/icons/gudock.png",
+      channelName: "채널",
+      views: "조회수",
+      uploaded: "5일 전"
+    }, {
+      videoId: "0poey19dA0c",
+      thumbnail: "https://i.ytimg.com/vi/0poey19dA0c/hqdefault.jpg",
+      title: "제목",
+      channelIcon: "/assets/icons/gudock.png",
+      channelName: "채널",
+      views: "조회수",
+      uploaded: "3일 전"
+    },
+    {
+      videoId: "-Q5O71sOM_0",
+      thumbnail: "https://i.ytimg.com/vi/-Q5O71sOM_0/hqdefault.jpg",
+      title: "제목",
+      channelIcon: "/assets/icons/gudock.png",
+      channelName: "채널",
+      views: "조회수",
+      uploaded: "5일 전"
+    },{
+      videoId: "-Q5O71sOM_0",
+      thumbnail: "https://i.ytimg.com/vi/-Q5O71sOM_0/hqdefault.jpg",
+      title: "제목",
+      channelIcon: "/assets/icons/gudock.png",
+      channelName: "채널",
+      views: "조회수",
+      uploaded: "5일 전"
+    },{
+      videoId: "-Q5O71sOM_0",
+      thumbnail: "https://i.ytimg.com/vi/-Q5O71sOM_0/hqdefault.jpg",
+      title: "제목",
+      channelIcon: "/assets/icons/gudock.png",
+      channelName: "채널",
+      views: "조회수",
+      uploaded: "5일 전"
+    },{
+      videoId: "-Q5O71sOM_0",
+      thumbnail: "https://i.ytimg.com/vi/-Q5O71sOM_0/hqdefault.jpg",
+      title: "제목",
+      channelIcon: "/assets/icons/gudock.png",
+      channelName: "채널",
+      views: "조회수",
+      uploaded: "5일 전"
+    },{
+      videoId: "-Q5O71sOM_0",
+      thumbnail: "https://i.ytimg.com/vi/-Q5O71sOM_0/hqdefault.jpg",
+      title: "제목",
+      channelIcon: "/assets/icons/gudock.png",
+      channelName: "채널",
+      views: "조회수",
+      uploaded: "5일 전"
+    },{
+      videoId: "-Q5O71sOM_0",
+      thumbnail: "https://i.ytimg.com/vi/-Q5O71sOM_0/hqdefault.jpg",
+      title: "제목",
+      channelIcon: "/assets/icons/gudock.png",
+      channelName: "채널",
+      views: "조회수",
+      uploaded: "5일 전"
+    },{
+      videoId: "-Q5O71sOM_0",
+      thumbnail: "https://i.ytimg.com/vi/-Q5O71sOM_0/hqdefault.jpg",
+      title: "제목",
+      channelIcon: "/assets/icons/gudock.png",
+      channelName: "채널",
+      views: "조회수",
+      uploaded: "5일 전"
+    },{
+      videoId: "-Q5O71sOM_0",
+      thumbnail: "https://i.ytimg.com/vi/-Q5O71sOM_0/hqdefault.jpg",
+      title: "제목",
+      channelIcon: "/assets/icons/gudock.png",
+      channelName: "채널",
+      views: "조회수",
       uploaded: "5일 전"
     },
+    {
+      videoId: "pOvKK9DF_Wo",
+      thumbnail: "https://i.ytimg.com/vi/pOvKK9DF_Wo/hqdefault.jpg",
+      title: "제목",
+      channelIcon: "/assets/icons/gudock.png",
+      channelName: "채널",
+      views: "조회수",
+      uploaded: "1주 전"
+    }
   ];
 
-  videoDataList.forEach(video => {
+  videoDataList.forEach((video, index) => {
     const card = template.content.cloneNode(true);
 
     card.querySelector(".thumbnail").src = video.thumbnail;
@@ -38,57 +140,57 @@ export function loadVideoCards() {
     card.querySelector(".channel-name").textContent = video.channelName;
     card.querySelector(".video-meta").textContent = `${video.views} · ${video.uploaded}`;
 
+    card.querySelector(".video-card").addEventListener("click", () => {
+      window.location.href = `/pages/video.html?videoId=${video.videoId}`;
+    });
+
     videoGrid.appendChild(card);
   });
 }
+
 
 export function loadShortsCards() {
   const shortsRow = document.getElementById("shorts-row");
   const template = document.getElementById("shorts-card-template");
 
   const shortsDataList = [
+    {
+      thumbnail: "https://i.ytimg.com/vi/_piHYoqMJUs/hqdefault.jpg",
+      title: "짧은 영상1",
+      views: "조회수 5.1만회",
+    },
+    {
+      thumbnail: "https://i.ytimg.com/vi/0poey19dA0c/hqdefault.jpg",
+      title: "짧은 영상2",
+      views: "조회수 3.2만회",
+    },
    {
-      thumbnail: "/assets/thumb/1.jpg",
-      title: "제목",
-      views: "조회수 128만회",
-
+      thumbnail: "https://i.ytimg.com/vi/_piHYoqMJUs/hqdefault.jpg",
+      title: "짧은 영상1",
+      views: "조회수 5.1만회",
     },
     {
-      thumbnail: "/assets/thumb/1.jpg",
-      title: "제목",
-      views: "조회수 128만회",
-
+      thumbnail: "https://i.ytimg.com/vi/0poey19dA0c/hqdefault.jpg",
+      title: "짧은 영상2",
+      views: "조회수 3.2만회",
+    },{
+      thumbnail: "https://i.ytimg.com/vi/_piHYoqMJUs/hqdefault.jpg",
+      title: "짧은 영상1",
+      views: "조회수 5.1만회",
     },
     {
-      thumbnail: "/assets/thumb/1.jpg",
-      title: "제목",
-      views: "조회수 128만회",
-
+      thumbnail: "https://i.ytimg.com/vi/0poey19dA0c/hqdefault.jpg",
+      title: "짧은 영상2",
+      views: "조회수 3.2만회",
     },{
-      thumbnail: "/assets/thumb/1.jpg",
-      title: "제목",
-      views: "조회수 128만회",
-
-    },{
-      thumbnail: "/assets/thumb/1.jpg",
-      title: "제목",
-      views: "조회수 128만회",
-
-    },{
-      thumbnail: "/assets/thumb/1.jpg",
-      title: "제목",
-      views: "조회수 128만회",
-
-    },{
-      thumbnail: "/assets/thumb/1.jpg",
-      title: "제목",
-      views: "조회수 128만회",
-
-    },{
-      thumbnail: "/assets/thumb/1.jpg",
-      title: "제목",
-      views: "조회수 128만회",
-
+      thumbnail: "https://i.ytimg.com/vi/_piHYoqMJUs/hqdefault.jpg",
+      title: "짧은 영상1",
+      views: "조회수 5.1만회",
+    },
+    {
+      thumbnail: "https://i.ytimg.com/vi/0poey19dA0c/hqdefault.jpg",
+      title: "짧은 영상2",
+      views: "조회수 3.2만회",
     },
   ];
 
